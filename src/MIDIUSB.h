48d87567111f86937881a18eb8c2d88e73f009cd
blekenbleu <blekenbleu@gmail.com>
2021-11-16 20:36:32 -0500

    only supports Esp32TinyUSB for Arduino USB MIDI

--- src/MIDIUSB.h
+++ src/MIDIUSB.h
@@ -1,15 +1,14 @@
 /*
-  Alternative MIDI USB class
+  Alternative Arduino MIDI USB class using Esp32TinyUSB
 */
 #ifndef TinyMIDIUSB_h
 #define TinyMIDIUSB_h
 
-#include "esptinyusb.h"        // Esp32TinyUSB/src
-#include "MIDIUSB_Defs.h"      // MIDIUSB/src/
+#include "esptinyusb.h"		// Esp32TinyUSB/src
+#include "MIDIUSB_Defs.h"	// MIDIUSB/src/
 #include "Tmidiusb.h"		// Esp32TinyUSB/src/
 
-// glue MIDI_Library to ESP32TinyUSB
-//class CMidiUSB:public MIDIusb
+// ESP32TinyUSB wrapper for MIDI_Library
 class CMidiUSB			// Esp32TinyUSB alternative to MidiUSB
 {
   MIDIusb Tmidi;		// instance Esp32TinyUSB object
@@ -20,13 +19,13 @@ class CMidiUSB			// Esp32TinyUSB alternative to MidiUSB
     void flush(void) { return; }
 };
 extern CMidiUSB MidiUSB;	// USB-MIDI/src/*.h expects this instance
+
 #define USBCON			// enable MIDI_Library/examples
 #define MIDIUSB_H		// disable Arduino MIDIUSB's src/MIDIUSB.h
 #define USB_MIDI_H		// disable Arduino USBMIDI's src/usbmidi.h
 #include "USB-MIDI.h"		// transport layer
 
-// MIDIusb is the class for ESP32TinyUSB;
-// CMidiUSB will use MIDIusb
+// CMidiUSB uses MIDIusb, a class from ESP32TinyUSB.
 #define TUSBMIDI_CREATE_DEFAULT_INSTANCE() \
 USBMIDI_CREATE_DEFAULT_INSTANCE(); \
 MIDIusb Tmidi; \
